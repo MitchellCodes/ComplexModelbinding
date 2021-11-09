0d987540d69c653ee03a02f7f5c8887c5784ed7f
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-11-09 03:53:03 -0800

    Set up repository pattern for Instructors

--- ComplexModelbinding/Startup.cs
+++ ComplexModelbinding/Startup.cs
@@ -1,4 +1,5 @@
 using ComplexModelbinding.Data;
+using ComplexModelbinding.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,7 @@ namespace ComplexModelbinding
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IInstructorRepository, InstructorRepository>();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
